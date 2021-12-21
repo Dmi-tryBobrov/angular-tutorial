eabfc543b035f4a10c706d726124dc452175efaf
Dmi-tryBobrov <dmitrybobrovig@gmail.com>
2021-12-21 22:25:24 +0300

    added responsive navbar and navigation

--- src/app/navigation-bar/menu-item.ts
+++ src/app/navigation-bar/menu-item.ts
@@ -1,6 +1,7 @@
 export interface MenuItem {
     label: string;
     icon: string;
+    link?: string;
     smallScreen: boolean;
     mediumScreen: boolean;
     largeScreen: boolean;
--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -1,21 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, Component, OnInit } from '@angular/core';
 import { MenuItem } from './menu-item';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation-bar',
   templateUrl: './navigation-bar.component.html',
   styleUrls: ['./navigation-bar.component.scss']
 })
-export class NavigationBarComponent implements OnInit {
+export class NavigationBarComponent implements OnInit, AfterContentChecked {
 
   menuItems: MenuItem[] = [
-    {
-      label: 'Sign In',
-      icon: 'login',
-      smallScreen: true,
-      mediumScreen: true,
-      largeScreen: true
-    },
     {
       label: 'About',
       icon: 'help',
@@ -51,11 +46,53 @@ export class NavigationBarComponent implements OnInit {
       mediumScreen: false,
       largeScreen: true
     },
+    {
+      label: 'Sign In',
+      icon: 'login',
+      link: '/auth',
+      smallScreen: true,
+      mediumScreen: true,
+      largeScreen: true
+    },
+    {
+      label: 'Sign Out',
+      icon: 'logout',
+      link: 'logout',
+      smallScreen: true,
+      mediumScreen: true,
+      largeScreen: true
+    },
   ];
-  
-  constructor() { }
 
-  ngOnInit(): void {
+  public isLoggedIn = false;
+  
+  constructor(private router: Router,
+              private auth: AuthService) { }
+
+  ngAfterContentChecked(): void {
+    this.isLoggedIn = this.auth.isLoggedIn();
+  }
+
+  ngOnInit(): void
+   {
+  }
+
+  navigate(link: string | undefined): void {
+    if(!link)
+      this.router.navigate(['**']);//redirect to PageNotFound
+    else if(link === 'logout'){
+      this.auth.logOut();
+    } else
+      this.router.navigate([link]);
+  }
+
+  checkLabels(label: string): boolean {
+      if(label === "Sign In" && this.isLoggedIn)
+        return false;
+      if(label === "Sign Out" && !this.isLoggedIn)
+        return false;
+      
+      return true;
   }
 
 }
